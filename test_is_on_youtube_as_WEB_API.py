7e6fad3775ae0ed3f1308f2277acebef2515bf84
433doro <doroktan@gmail.com>
2018-05-17 13:19:46 +0300

    edited the webAPI test file

--- test_is_on_youtube_as_WEB_API.py
+++ test_is_on_youtube_as_WEB_API.py
@@ -2,11 +2,17 @@ import is_on_youtube_as_WEB_API
 from webtest import TestApp
 
 
-if __name__ == '__main__':
+def test_collect_song_data_using_WEB_API():
 
     youtube_songs_information_collection_app = TestApp(is_on_youtube_as_WEB_API.app)
     response_object = youtube_songs_information_collection_app.get('/song/river/')
 
-    assert response_object.status_code == 200
+    assert response_object.status_code == 200, "First test succeeded"
 
+
+
+
+if __name__ == '__main__':
+
+    test_collect_song_data_using_WEB_API()
 
